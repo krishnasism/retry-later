cd020e6aca47de7770390d90ffd9811b8e454eb0
Krishnasis Mandal <krishnasismandal97@gmail.com>
2024-03-17 11:44:25 +0100

    remove async from normal demo

--- examples/demo.py
+++ examples/demo.py
@@ -7,18 +7,18 @@ loop = asyncio.new_event_loop()
 
 
 @retry_later(retry_interval=5, max_retries=3, backoff=2)
-async def my_function():
+def my_function():
     print("Running my_function")
     time.sleep(5)
     raise Exception("Function failed")
 
 
-async def main():
-    await my_function()
+def main():
+    my_function()
     print("Hello")
 
 
-asyncio.run(main())
+main()
 
 print("Hi - I am here!! I complete before my_function!")
 
